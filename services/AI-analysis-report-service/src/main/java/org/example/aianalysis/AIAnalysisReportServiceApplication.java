104b4541006fd22fb6424c9c38aae0694c276ebf
cdz_hy <3116362036@qq.com>
2025-11-15 18:54:08 +0800

    完善微服务的注册和启动；

---
+++ services/AI-analysis-report-service/src/main/java/org/example/aianalysis/AIAnalysisReportServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.aianalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIAnalysisReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AIAnalysisReportServiceApplication.class, args);
+    }
+
+}
---
+++ services/diet-record-service/src/main/java/org/example/dietrecord/DietRecordServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.dietrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietRecordServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietRecordServiceApplication.class, args);
+    }
+
+}

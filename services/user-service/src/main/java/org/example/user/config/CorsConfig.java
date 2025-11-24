f26f05f9b7102f0ff9c1712f951d92e0a18eca0a
壹果 <15567349+unique-fruit@user.noreply.gitee.com>
2025-11-24 22:58:47 +0800

    跨域问题解决

--- services/user-service/src/main/java/org/example/user/config/CorsConfig.java
+++ services/user-service/src/main/java/org/example/user/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package org.example.user.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConditionalOnProperty(name = "user.cors.enabled", havingValue = "true")
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
